befff66570d6d6e8068fbcc4e0c524ab6e1a57d9
Thatsvie <viepaulalmt@gmail.com>
2025-02-02 16:09:55 -0600

    fix: Correct simulated page loading delay

--- app/ui/[id]/page.tsx
+++ app/ui/[id]/page.tsx
@@ -1,9 +1,10 @@
-import { notFound } from "next/navigation";
+async function simulateLoading() {
+  return new Promise((resolve) => setTimeout(resolve, 3000));
+}
 
-export default function Page({ params }: { params: Record<string, string> }) {
-  if (!params?.id) {
-    return notFound();
-  }
+export default async function Page({ params }: { params: { id: string } }) {
+  // Simulate slow page loading
+  await simulateLoading();
 
   return <div>Topic Page: {params.id}</div>;
 }
