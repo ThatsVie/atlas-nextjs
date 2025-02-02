1f190a36859038e2b8c43facd22495388bf78f5b
Thatsvie <viepaulalmt@gmail.com>
2025-02-02 16:02:34 -0600

    fix: Resolve type issue in dynamic route

--- app/ui/[id]/page.tsx
+++ app/ui/[id]/page.tsx
@@ -1,16 +1,13 @@
 import { notFound } from "next/navigation";
 
-export default async function Page({
-  params,
-}: {
-  params: { id: string };
-}) {
+interface PageProps {
+  params: { id?: string };
+}
+
+export default function Page({ params }: PageProps) {
   if (!params?.id) {
     return notFound();
   }
-
-  // Simulate Page loading
-  await new Promise((r) => setTimeout(r, 3000));
 
   return <div>Topic Page: {params.id}</div>;
 }
