29705c4349284ad9011843362b3ef0c6250bea2c
Thatsvie <viepaulalmt@gmail.com>
2025-02-02 15:57:24 -0600

    fix: Correct TypeScript error in dynamic route

--- app/ui/[id]/page.tsx
+++ app/ui/[id]/page.tsx
@@ -1,6 +1,16 @@
-export default async function Page({ params }: { params: { id: string } }) {
-    //Simulate Page loading
-    await new Promise((r) => setTimeout(r, 3000));
-  
-    return <div>Topic Page: {params.id}</div>;
+import { notFound } from "next/navigation";
+
+export default async function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
+  if (!params?.id) {
+    return notFound();
   }
+
+  // Simulate Page loading
+  await new Promise((r) => setTimeout(r, 3000));
+
+  return <div>Topic Page: {params.id}</div>;
+}
