5d7850a9813b70584da2cde781c3094ae5e0e381
Thatsvie <viepaulalmt@gmail.com>
2025-02-02 16:16:25 -0600

    chore: Disable strict mode to resolve Next.js param type issue

--- app/ui/[id]/page.tsx
+++ app/ui/[id]/page.tsx
@@ -1,10 +1,6 @@
-async function simulateLoading() {
-  return new Promise((resolve) => setTimeout(resolve, 3000));
-}
-
 export default async function Page({ params }: { params: { id: string } }) {
-  // Simulate slow page loading
-  await simulateLoading();
+  //Simulate Page loading
+  await new Promise((r) => setTimeout(r, 3000));
 
   return <div>Topic Page: {params.id}</div>;
 }
